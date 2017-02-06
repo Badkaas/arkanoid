795f71f5789457c7d7093fbcfbbacb0a6608938d
Despo <Despo@despobadkaas.home>
2017-02-06 02:07:27 +0100

    Add many features (title, trainig, save position) and clean code

--- arkanoid.py
+++ arkanoid.py
@@ -66,9 +66,8 @@ for n in range (BLOCKS_GRID):
     block = Block(_x, _y, BLOCK_W, BLOCK_H)
     blocks.append((_x, _y))
 
-# __
 
-cur_blocks = blocks.copy()
+# __
 
 game_text = lambda _font, _str, _color: _font.render( _str , 1, _color)
 
@@ -82,7 +81,6 @@ def main(score=0, life=4, char_right=None,
     screen = pygame.display.set_mode((SURFACE_W, SURFACE_H))
     pygame.display.set_caption('Arkanoid')
 
-
     # Fill background
     background = pygame.Surface(screen.get_size())
     background = background.convert()
@@ -92,6 +90,14 @@ def main(score=0, life=4, char_right=None,
     char.centerx = background.get_rect().centerx
     ball.centerx = background.get_rect().centerx
 
+    cur_blocks = blocks.copy()
+
+    # Game's text
+    title_str = 'arkanoid'.capitalize()
+    title = game_text(large_font, title_str, GRAY)
+    title_pos = title.get_rect()
+    title_pos.centerx = background.get_rect().centerx
+    title_pos.centery = background.get_rect().centery
 
     rule_str = "Press q or d from right to left p for game and Esc for quit."
 
@@ -119,9 +125,14 @@ def main(score=0, life=4, char_right=None,
     screen.blit(background, (0, 0))
     pygame.display.flip()
 
+    # Training Mode Save System
+    save_blocks = []
+    save_game_pos = (ball.y, ball.x, ball_v, ball_h, char.x)
+
     # Event loop
     game_over = False
     death = False
+    start = True
 
     while not game_over:
 
@@ -132,48 +143,47 @@ def main(score=0, life=4, char_right=None,
         events = pygame.event.get()
         for event in events:
 
-            if event.type is KEYUP and event.key == K_ESCAPE \
-               or event.type is pygame.QUIT:
-                game_over = True
+            if event.type == KEYDOWN and event.key in (K_q, K_d):
+                char_right = event.key == K_d
+            elif event.type == KEYUP:
+                if event.key == K_ESCAPE:
+                    game_over = True
+                elif event.key in (K_q, K_d):
+                    char_right = None
+                elif event.key == K_p:
+                    if not start:
+                        save_game_pos = (ball.y, ball.x, ball_v, ball_h, char.x)
+                        save_blocks = cur_blocks.copy()
 
-            # Detect events
-            # ------------------------------------------------------
+                    else:
+                        cur_blocks = save_blocks.copy()
 
-            elif event.type == KEYDOWN and event.key == K_q:
-                char_right = False
-            elif event.type == KEYUP and event.key == K_q:
-                char_right = None
-            elif event.type == KEYDOWN and event.key == K_d:
-                char_right = True
-            elif event.type == KEYUP and event.key == K_d:
-                char_right = None
-            elif event.type == KEYUP and event.key == K_p:
+                        ball.y, ball.x, ball_v, ball_h, char.x = save_game_pos 
+                        save_blocks.clear()
 
-                if death:
-                    death = not death
-                    cur_life = life
-                    score = 0
-                    ball_v = False
-                    ball_h = None
-                    ball.y = BALL_Y
-                    ball.centerx = background.get_rect().centerx
+                    if not death:
+                        start = not start
+                    else:
+                        start = False
+                        death = not death
+                        cur_life = life
+                        score = 0
+                        ball_v = False
+                        ball_h = None
+                        ball.y = BALL_Y
+                        ball.centerx = background.get_rect().centerx
 
             elif event.type == USEREVENT:
                 score_str = "Score: {:05d}".format(score)
                 score_text = game_text(medium_font, score_str, GRAY)
 
-
-                # fps_str = '{:.2f} ({})'.format(timer.get_fps(), dt)
-                # fps_text = game_text(medium_font, fps_str, GRAY)
-                pass
+            elif event.type == pygame.QUIT:
+                game_over = true
 
         # Update scene
         # --------------------------------------------------------------
-
-        if not death and score ==0:
+        if not start and score == 0:
             cur_blocks = blocks.copy()
-
-
 
         if ball_v is not None:
             ball_y_motion = new_pos(ball.y, dt, increase=ball_v)
@@ -182,13 +192,17 @@ def main(score=0, life=4, char_right=None,
                 ball.centerx = SURFACE_W // 2
                 ball.y = BALL_Y
                 ball_h = None
-                cur_life -= 1
+                if not start:
+                    cur_life -= 1
 
                 if cur_life == 0:
                     death = not death
 
+
                 if not death:
                     ball_v = False
+                if start:
+                    ball_v = True
 
             elif ball_y_motion < MARGIN_TOP:
                 ball_v = not ball_v
@@ -211,7 +225,7 @@ def main(score=0, life=4, char_right=None,
                 ball.x = ball_x_motion
 
         if char.colliderect(ball):
-            if not death:
+            if not death and not start:
                 score += 10
             if ball_v:
                 ball_v = not ball_v
@@ -223,8 +237,8 @@ def main(score=0, life=4, char_right=None,
             block = Block(_x,_y , BLOCK_W, BLOCK_H)
             if ball.colliderect(block):
                 cur_blocks.remove((block.x,block.y))
-                if not death:
-                    score += 100
+                if not death and not start:
+                        score += 100
 
                 if ball.top < block.top or ball.bottom > block.bottom:
                     if ball.x < block.x:
@@ -242,7 +256,8 @@ def main(score=0, life=4, char_right=None,
                     else:
                         ball_v = not ball_v
                         ball.top = block.bottom + 1
-                    break
+
+                        break
 
                 else:
                     ball_h = not ball_v
@@ -251,25 +266,25 @@ def main(score=0, life=4, char_right=None,
             char_x_motion = new_pos(char.x, dt, increase=char_right)
 
             if char_x_motion < MARGIN:
-                if not death:
-                    char.x = MARGIN + 1
+                char.x = MARGIN + 1
+
             elif char_x_motion > SURFACE_W - MARGIN - CHAR_W:
                 char.x = SURFACE_W - MARGIN - CHAR_W - 1
+
             else:
                 char.x = char_x_motion
 
         # Draw screen
         # --------------------------------------------------------------
         screen.blit(background, (0, 0))
+        if not start:
+            for n in range (cur_life):
+                _x = score_text_pos.x - (n * (LIFE_CURSOR_W + 2)) - LIFE_CURSOR_W - 2
+                life_cursor = LifeCursor(_x, LIFE_OFFSET_Y, LIFE_CURSOR_W, LIFE_CURSOR_H)
+                pygame.draw.rect(screen, RED, life_cursor)
 
-        for n in range (cur_life):
-            _x = score_text_pos.x - (n * (LIFE_CURSOR_W + 2)) - LIFE_CURSOR_W - 2
-            life_cursor = LifeCursor(_x, LIFE_OFFSET_Y, LIFE_CURSOR_W, LIFE_CURSOR_H)
-            pygame.draw.rect(screen, RED, life_cursor)
-
-        #text
-        screen.blit(score_text, score_text_pos)
-        # screen.blit(fps_text, fps_text_pos)
+                #text
+                screen.blit(score_text, score_text_pos)
 
         #ball
         pygame.draw.rect(screen, RED, ball)
@@ -282,9 +297,14 @@ def main(score=0, life=4, char_right=None,
             block = Block(xE, yE, BLOCK_W, BLOCK_H)
             pygame.draw.rect(screen, BLACK, block)
 
+        if start:
+            screen.blit(title, title_pos)
+            screen.blit(rule, rule_pos)
+
         if death:
             screen.blit(loser_text, loser_text_pos)
             screen.blit(rule, rule_pos)
+
 
         # Blit everything to the screen
         # --------------------------------------------------------------
